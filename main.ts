958b23d268242458264f5ab5053d5b8a773d70c2
MateoMT <49064180+MateoMT@users.noreply.github.com>
2022-01-15 00:25:58 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,123 +1,259 @@
 /**
- * Functions are mapped to blocks using various macros
- * in comments starting with %. The most important macro
- * is "block", and it specifies that a block should be
- * generated for an **exported** function.
+ * Organize your blocks in groups
  */
-enum on_off{
-    //%block="on"
-    turnon,
-    //%block="off"
-    turnoff
-}
-enum timerunning{
-    //% block="1 minute"
-    oneminute,
-    //% block="30 minutes"
-    thirtyminutes,
-    //% block="35 minutes"
-    thirtyfiveminutes,
-    //% block="40 minutes"
-    fortyminutes,
-    //% block="45 minutes"
-    fortyfiveminutes,
-    //% block="50 minutes"
-    fiftyminutes,
-    //% block="55 minutes"
-    fiftyfiveminutes,
-    //% block="60 minutes"
-    sixtyminutes 
-}
-//% color="#fec636" weight=100 icon="\uf0e7"
-namespace electrophoresis {
 
-    
-     //% block="Turn %start the electrophoresis" blockGap=8
-     //% weight=100
-     export function start_electrophoresis(start:on_off):void{
-	pixel.setColor(0xff0000);//rojo
-	pause(5000);
-        	switch (start){
-	   		case on_off.turnon: {
-				pixel.setColor(0x007fff);//azul
-				pause(5000);
-				pins.D34.digitalWrite(false); 
-				pause(5000);
-				break;
-			}
-            		case on_off.turnoff: {
-				pixel.setColor(0xffff00);//amarillo
-				pause(5000);	
-           			pins.D34.digitalWrite(true);
-				pause(5000);
-				pixel.setColor(0xffffff);//BLANCO
-				break;
-			}
-        	}
-          
-     }
-     
+//% color="#AA278D"
+//% groups="['Book-keeping instructions', 'Timing','Symbolic manipulation','Disposal','Declaration of resources','Measurement','Combination & mixing','Temperature & storage','Centrifugation','Detection and analysis']"
+namespace BioBlocks {
+    //% block="start_protocol protocol_name= $protocol"
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function start_protocol(protocol: string) {
 
-    /* @param start, turn on/off the electrophoresis eg:on_off.turnon
-    /* @param time, electrophoresis time eg: timerunning.thirtyminutes
-    */
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function end_protocol() {
 
-	//% block="Run the electrophoresis during %time" blockGap=16
-    //% weight=90
-    export function time_electrophoresis(time: timerunning): void {
-    	switch (time){
-		case timerunning.oneminute: {
-			pixel.setColor(0xff0000);//rojo
-			pins.D35.digitalWrite(false);
-			pause(60*1000);
-			pixel.setColor(0xffff00);//amarillo
-			break;
-		}
-		
-		case timerunning.thirtyminutes: {
-			pins.D35.digitalWrite(false);
-			pause(30*60*1000);
-			break;
-		}
-		case timerunning.thirtyfiveminutes: {
-			pins.D35.digitalWrite(false);
-			pause(35*60*1000);
-			break;
-		}
-		case timerunning.fortyminutes: {
-			pins.D35.digitalWrite(false);
-			pause(40*60*1000);
-			break;
-		}
-		case timerunning.fortyfiveminutes: {
-			pins.D35.digitalWrite(false);
-			pause(45*60*1000);
-			break;
-		}
-		case timerunning.fiftyminutes: {
-			pins.D35.digitalWrite(false);
-			pause(50*60*1000);
-			break;
-		}
-		case timerunning.fiftyfiveminutes: {
-			pins.D35.digitalWrite(false);
-			pause(55*60*1000);
-			break;
-		}
-		case timerunning.sixtyminutes: {
-			pins.D35.digitalWrite(false);
-			pause(60*60*1000);
-			break;
-		}	   
-    	} 
-	
-    	pins.D35.digitalWrite(true);
-	pause(5000);
-	pixel.setColor(0x007fff);//azul
-	
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function first_step() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function next_step() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function optional_step() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function first_option() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function next_option() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function end_option() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function first_sub_step() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function next_sub_step() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function parallel_step() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function comment() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function to_do() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function repeat() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function name_sample() {
+
+    }   
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function name_container() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function name_plate() {
+
+    }
+   
+    //% block
+    //% group="Timing"
+    //% color="#F978A3"
+    export function wait() {
+
+    }
+    //% block
+    //% group="Timing"
+    //% color="#F978A3"
+    export function store_until() {
+
+    }
+    //% block
+    //% group="Timing"
+    //% color="#F978A3"
+    export function use_or_store() {
+
+    }
+    //% block
+    //% group="Timing"
+    //% color="#F978A3"
+    export function time_constraint() {
+
+    }
+   
+    //% block
+    //% group="Symbolic manipulation"
+    //% color="#F7A4FA"
+    export function set_value() {
+
+    }
+    //% block
+    //% group="Symbolic manipulation"
+    //% color="#F7A4FA"
+    export function assign() {
+
+    }
+    //% block
+    //% group="Symbolic manipulation"
+    //% color="#F7A4FA"
+    export function add() {
+
+    }
+    //% block
+    //% group="Symbolic manipulation"
+    //% color="#F7A4FA"
+    export function divede() {
+
+    }
+    //% block
+    //% group="Symbolic manipulation"
+    //% color="#F7A4FA"
+    export function subtract() {
+
+    }
+    //% block
+    //% group="Symbolic manipulation"
+    //% color="#F7A4FA"
+    export function multiply() {
+
+    }
+ 
+    //% block
+    //% group="Disposal"
+    export function discard() {
+
+    }
+    //% block
+    //% group="Disposal"
+    export function drain() {
+
     }
 
-    // note that Caml casing yields lower case
-    // block text with spaces
+    //% block
+    //% group="Declaration of resources"
+    //% color="#9170FC"
+    export function new_fluid() {
 
+    }
+    //% block
+    //% group="Declaration of resources"
+    //% color="#9170FC"
+    export function new_solid() {
+
+    }
+    //% block
+    //% group="Declaration of resources"
+    //% color="#9170FC"
+    export function new_container() {
+
+    }
+    //% block
+    //% group="Declaration of resources"
+    //% color="#9170FC"
+    export function new_plate() {
+
+    }
+    //% block
+    //% group="Declaration of resources"
+    //% color="#9170FC"
+    export function new_slide() {
+
+    }
+    //% block
+    //% group="Declaration of resources"
+    //% color="#9170FC"
+    export function new_symbol() {
+
+    }
+    //% block
+    //% group="Declaration of resources"
+    //% color="#9170FC"
+    export function new_column() {
+
+    }
+
+    //% block
+    //% group="Measurement"
+    export function measure_fluid() {
+
+    }
+ 
+    //% block
+    //% group="Combination & mixing"
+    export function combine() {
+
+    }
+    
+
+    //% block
+    //% group="Temperature & storage"
+    export function incubate() {
+
+    }
+    
+    //% block
+    //% group="Centrifugation"
+    export function centrifuge() {
+
+    }
+
+    //% block
+    //% group="Detection & analysis"
+    export function ce_detect() {
+
+    }
+ 
 }
+
