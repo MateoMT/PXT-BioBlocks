b6b040a2148352cc5bec66877f9401887aeacead
MateoMT <49064180+MateoMT@users.noreply.github.com>
2022-01-15 00:22:58 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,260 @@
+/**
+ * Organize your blocks in groups
+ */
+
+//% color="#AA278D"
+//% groups="['Book-keeping instructions', 'Timing','Symbolic manipulation','Disposal','Declaration of resources','Measurement','Combination & mixing','Temperature & storage','Centrifugation','Detection and analysis']"
+namespace BioBlocks {
+    //% block="start_protocol protocol_name= $protocol"
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function start_protocol(protocol: string) {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function end_protocol() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function first_step() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function next_step() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function optional_step() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function first_option() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function next_option() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function end_option() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function first_sub_step() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function next_sub_step() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function parallel_step() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function comment() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function to_do() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function repeat() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function name_sample() {
+
+    }   
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function name_container() {
+
+    }
+    //% block
+    //% group="Book-keeping instructions"
+    //% color='#FF8900'
+    export function name_plate() {
+
+    }
+   
+    //% block
+    //% group="Timing"
+    //% color="#F978A3"
+    export function wait() {
+
+    }
+    //% block
+    //% group="Timing"
+    //% color="#F978A3"
+    export function store_until() {
+
+    }
+    //% block
+    //% group="Timing"
+    //% color="#F978A3"
+    export function use_or_store() {
+
+    }
+    //% block
+    //% group="Timing"
+    //% color="#F978A3"
+    export function time_constraint() {
+
+    }
+   
+    //% block
+    //% group="Symbolic manipulation"
+    //% color="#F7A4FA"
+    export function set_value() {
+
+    }
+    //% block
+    //% group="Symbolic manipulation"
+    //% color="#F7A4FA"
+    export function assign() {
+
+    }
+    //% block
+    //% group="Symbolic manipulation"
+    //% color="#F7A4FA"
+    export function add() {
+
+    }
+    //% block
+    //% group="Symbolic manipulation"
+    //% color="#F7A4FA"
+    export function divede() {
+
+    }
+    //% block
+    //% group="Symbolic manipulation"
+    //% color="#F7A4FA"
+    export function subtract() {
+
+    }
+    //% block
+    //% group="Symbolic manipulation"
+    //% color="#F7A4FA"
+    export function multiply() {
+
+    }
+ 
+    //% block
+    //% group="Disposal"
+    export function discard() {
+
+    }
+    //% block
+    //% group="Disposal"
+    export function drain() {
+
+    }
+
+    //% block
+    //% group="Declaration of resources"
+    //% color="#9170FC"
+    export function new_fluid() {
+
+    }
+    //% block
+    //% group="Declaration of resources"
+    //% color="#9170FC"
+    export function new_solid() {
+
+    }
+    //% block
+    //% group="Declaration of resources"
+    //% color="#9170FC"
+    export function new_container() {
+
+    }
+    //% block
+    //% group="Declaration of resources"
+    //% color="#9170FC"
+    export function new_plate() {
+
+    }
+    //% block
+    //% group="Declaration of resources"
+    //% color="#9170FC"
+    export function new_slide() {
+
+    }
+    //% block
+    //% group="Declaration of resources"
+    //% color="#9170FC"
+    export function new_symbol() {
+
+    }
+    //% block
+    //% group="Declaration of resources"
+    //% color="#9170FC"
+    export function new_column() {
+
+    }
+
+    //% block
+    //% group="Measurement"
+    export function measure_fluid() {
+
+    }
+ 
+    //% block
+    //% group="Combination & mixing"
+    export function combine() {
+
+    }
+    
+
+    //% block
+    //% group="Temperature & storage"
+    export function incubate() {
+
+    }
+    
+    //% block
+    //% group="Centrifugation"
+    export function centrifuge() {
+
+    }
+
+    //% block
+    //% group="Detection & analysis"
+    export function ce_detect() {
+
+    }
+ 
+}
+
 
