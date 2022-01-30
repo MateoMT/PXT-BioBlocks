697f758cfd977cef1350c27529b0e167e6cf6bcb
MateoMT <49064180+MateoMT@users.noreply.github.com>
2022-01-30 21:59:03 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -202,7 +202,7 @@ enum Time_constraint_type{
 
 
 
-//% color="#AA278D" icon="\f0c3"
+//% weight=100 color=#FF5733 icon="\uf021"
 //% groups="['Book-keeping instructions', 'Timing','Symbolic manipulation','Disposal','Declaration of resources','Measurement','Combination & mixing','Temperature & storage','Centrifugation','Detection and analysis']"
 namespace BioBlocks {
     //% block="%protocol=variables_get(protocol_name)"
